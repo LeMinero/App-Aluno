b78727e39fa1788ab0f010f406af8a095e2809bf
LeMinero <gustavofariamelo@gmail.com>
2026-08-19 19:41:59 -0300

    feat: Correção do template, cadastro de admin e migrações

--- aluno/models.py
+++ aluno/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+ 
+ 
 class Aluno(models.Model):
     nome = models.CharField(max_length=100)
     curso = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
-
-    def _str_(self):
+ 
+    def __str__(self):
         return self.nome
